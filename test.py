b751d3c48666f3e6f2d7f38620eb0363db191fbd
Leo <shiyu_l7@163.com>
2024-05-18 16:30:04 +0800

    test base_run

--- test.py
+++ test.py
@@ -31,8 +31,11 @@ img_transform = transforms.Compose([
 ])
 
 # 加载测试数据
+xy = torch.zeros(1 ,4, 17, 17)
 test_mat_file = 'raw/hbp01.mat'
-data = hdf5storage.loadmat(test_mat_file, variable_names={'frame'})
+data = hdf5storage.loadmat(test_mat_file, variable_names={'frame', 'jointsMatrix'})
+joints_matrix = data['jointsMatrix'].transpose() # 17*17
+xy[0, :, :, :] = torch.from_numpy(joints_matrix).type(torch.FloatTensor)
 frame = data['frame']
 
 # 加载并预处理图片
@@ -45,28 +48,25 @@ img = Image.open(img_file_path).convert("RGB")
 img = img_transform(img)
 img = img.unsqueeze(0)  # 添加批次维度
 
-# 转换为Variable并移到GPU
 csi_data = Variable(img.cuda())
 
 # 推理
 with torch.no_grad():
     pred_xy = unet_model(csi_data)
 
-# 将预测结果转换为numpy数组
 pred_xy = pred_xy.cpu().numpy()
+# pred_xy = xy
 
-# 提取关节点坐标
 poseVector_x = np.zeros((17,))
 poseVector_y = np.zeros((17,))
 for index in range(17):
     poseVector_x[index] = pred_xy[0, 0, index, index]
     poseVector_y[index] = pred_xy[0, 1, index, index]
 
-# 显示图像并绘制关节点和肢体连接
-plt.imshow(cv2.resize(frame, (1280, 720)))
+# plot
+plt.imshow(frame)
 for i in range(len(limb)):
-    plt.plot(poseVector_x[[limb[i, 0], limb[i, 1]]]* frame.shape[1] / 256, poseVector_y[[limb[i, 0], limb[i, 1]]]* frame.shape[0] / 256, marker='o')
+    plt.plot(poseVector_x[[limb[i, 0], limb[i, 1]]], poseVector_y[[limb[i, 0], limb[i, 1]]], marker='o')
 plt.show()
 
-# 确保cv2窗口关闭
 cv2.destroyAllWindows()
